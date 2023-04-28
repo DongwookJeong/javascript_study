8872325635fa806870c314dd553e39cf2b931953
DongwookJeong <nolhoon211@gmail.com>
2023-04-29 01:46:20 +0900

    async.await Promise를 쉽게 사용할 수 있도록 해주는 문법

--- call.js
+++ call.js
@@ -9,29 +9,27 @@ function increase(number){
                 return reject(e)
             }
             resolve(result); // ! number 값에 +10 후 성공 처리
-        }, 3000)
+        }, 1000)
     })
     return promise
 }
-increase(0)
-    .then(number => {
-        // ! Promise에서 resolve된 값은 .then을 통해 받아 올 수 있음
-        console.log(number)
-        return increase(number) // ! Promise를 리턴하면
-    })
-    .then(number => {
-        console.log(number)
-        return increase(number)
-    })
-    .then(number => {
-        console.log(number)
-        return increase(number)
-    })
-    .then(number => {
-        console.log(number)
-        return increase(number)
-    })
-    .catch(e => {
-        // ! 도중에 에러가 발생한다면 .catch를 통해 알 수 있음
-        console.log(e)
-    })
+async function runTasks(){
+    try { // ! try/catch 구문을 사용하여 에러를 처리
+        let result = await increase(0)
+        console.log(result)
+        result = await increase(result)
+        console.log(result)
+        result = await increase(result)
+        console.log(result)
+        result = await increase(result)
+        console.log(result)
+        result = await increase(result)
+        console.log(result)
+        result = await increase(result)
+        console.log(result)
+        result = await increase(result)
+        console.log(result)
+    }catch(e) {
+        console.log(e);
+    }
+}
